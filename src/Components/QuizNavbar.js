c7ef3c07ef41a836ea0c206718a171710e2dbe8d
hehn <sarah.hehn@adesso.de>
2024-02-09 14:09:52 +0100

    Added logos to the navbar

--- src/Components/QuizNavbar.js
+++ src/Components/QuizNavbar.js
@@ -5,34 +5,19 @@ import {
     NavDropdown,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {AiFillPicture, AiFillQuestionCircle, AiOutlineLogout, AiOutlineMedium, AiOutlineMessage} from "react-icons/ai";
 
 
 function QuizNavbar() {
     return (
-        <div className="App container">
+        <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">
-                            Something
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="#action/3.4">
-                            Separated link
-                        </NavDropdown.Item>
-                    </NavDropdown>
+                    <AiFillPicture/>
+                    <AiOutlineMessage/>
+                    <AiFillQuestionCircle/>
+                    <AiOutlineLogout/>
                     <Navbar.Brand href="#home">ISEF QUIZ</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
