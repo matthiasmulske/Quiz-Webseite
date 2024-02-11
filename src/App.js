d2e8b06192e36a342863762375aff8d82f439ad2
matthiasmulske <matthias.mulske@web.de>
2024-02-11 15:09:47 +0100

    new Headline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Button from "./Components/Button";
-
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Button from "./Components/Button";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
