6e4384e77a3af03d42654717c5534472151a32b2
matthiasmulske <matthias.mulske@web.de>
2024-02-11 13:10:41 +0100

    new Headline

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Button from "./Components/Button";
+
 function App() {
   return (
     <div className="App">
---
+++ src/Components/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Button() {
+    return (
+      <div className="App">
+        <Button>Einzelspieler</Button>
+        <Button>Mehrspieler</Button>
+      </div>
+    );
+  }
+  
+  export default Button;
