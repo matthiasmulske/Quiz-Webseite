39c37636159643fddb41155a58aeeaf371f56326
hehn <sarah.hehn@adesso.de>
2024-02-12 13:50:42 +0100

    rearranging

--- src/Components/QuizNavbar.js
+++ src/Components/QuizNavbar.js
@@ -1,28 +1,57 @@
-import {
-    Container,
-    Nav,
-    Navbar,
-    NavDropdown,
-} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {AiFillPicture, AiFillQuestionCircle, AiOutlineLogout, AiOutlineMedium, AiOutlineMessage} from "react-icons/ai";
+import {AiFillPicture, AiFillQuestionCircle, AiOutlineLogout, AiOutlineMessage} from "react-icons/ai"; // Import Bootstrap components
+
+import { Navbar, Container } from 'react-bootstrap';
+import React from "react"; // Import Bootstrap components
 
 
 function QuizNavbar() {
     return (
-        <div>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <AiFillPicture/>
-                    <AiOutlineMessage/>
-                    <AiFillQuestionCircle/>
-                    <AiOutlineLogout/>
-                    <Navbar.Brand href="#home">ISEF QUIZ</Navbar.Brand>
-                </Container>
-            </Navbar>
+        <div style={style.container_navbar}>
+            <ul style={style.icon_group}>
+                <a href={"#profile"}>
+                    <AiFillPicture style={style.icon}/>
+                </a>
+                <a href={"#messages"}>
+                    <AiOutlineMessage style={style.icon}/>
+                </a>
+                <a href={"#help"}>
+                    <AiFillQuestionCircle style={style.icon}/>
+                </a>
+                <a href={"#logout"}>
+                    <AiOutlineLogout style={style.icon}/>
+                </a>
+            </ul>
+            <div>
+                <a href={"#logout"}>
+                    <AiOutlineLogout style={style.logo}/>
+                </a>
+            </div>
         </div>
-    )
 
+    );
 }
+
+const style = {
+    container_navbar: {
+        fontSize: '1.9rem',
+    },
+
+    icon_group: {
+        float: 'left',
+        color: 'rgb(0, 0, 0.8)',
+    },
+
+    icon: {
+        color: 'rgb(0, 0, 0.8)',
+    },
+
+    logo: {
+        float: 'right',
+        paddingTop: '10px',
+    },
+
+
+};
+
 
 export default QuizNavbar;
