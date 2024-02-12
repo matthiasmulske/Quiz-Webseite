a751083c18cd5d9c2d739f49e196db22a2c8e2be
hehn <sarah.hehn@adesso.de>
2024-02-12 17:35:41 +0100

    created files; added dependecny fpr material ui

---
+++ src/Atoms/Button.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function Button() {
+    return (
+        <>
+            <Button variant="contained">test</Button>;
+        </>
+    )
+
+}
+
+export default Button;
