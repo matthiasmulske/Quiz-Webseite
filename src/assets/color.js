b8fccdefae3b97a1dc065912dea874bdb91e0989
LW <lucaswankerl8@gmail.com>
2024-02-16 10:02:00 +0100

    new file:   src/assets/color.js 	deleted:    src/assets/test,js

---
+++ src/assets/color.js
@@ -0,0 +1,10 @@
+const colors = {
+    primary: "rgb(13, 110, 253)",
+    secondary: "rgb(108, 117, 125)",
+    success: "rgb(25, 135, 84)",
+    info: "rgb(13, 202, 240)",
+    warning: "rgb(255, 193, 7)",
+    danger: "rgb(220, 53, 69)",
+    light: "rgb(248, 249, 250)",
+    dark: "rgb(33, 37, 41)"
+}
