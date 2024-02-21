a11177905bd1b7262b02eb8cd1689d7f0a1b86c6
LW <lucaswankerl8@gmail.com>
2024-02-21 16:32:27 +0100

    modified:   src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,49 +10,50 @@ import MailIcon from '@mui/icons-material/Mail';
 import logo from './../assets/logo-removebg-preview.png';
 import LogoutIcon from '@mui/icons-material/Logout';
 
-export default function PrimarySearchAppBar(loggedIn) {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <img src={logo} alt="logo" height="50px"></img>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                    >
-                        ISEF QUIZ
-                    </Typography>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Box >
-                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                            <Badge badgeContent={4} color="error">
-                                <MailIcon />
-                            </Badge>
-                        </IconButton>
-                        <IconButton
-                            size="large"
-                            edge="end"
-                            aria-label="account of current user"
-                            aria-haspopup="true"
-                            //   onClick={}
-                            color="inherit"
-                        >
-                            <Badge badgeContent="user" color="secondary">
-                                <AccountCircle />
-                            </Badge>
-                        </IconButton>
-                        <IconButton
-                            size="large"
-                            edge="end"
-                            //   onClick={}
-                            color="inherit"
-                        >
-                            <LogoutIcon />
-                        </IconButton>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+export default function PrimarySearchAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+            <img src={logo} alt="logo" height="50px"></img>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            ISEF QUIZ
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box >
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+            //   onClick={}
+              color="inherit"
+            >
+                <Badge badgeContent="user" color="secondary">
+              <AccountCircle />
+                </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+            //   onClick={}
+              color="inherit"
+            >
+              <LogoutIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
