7732c1c1a66024b96a7ef9664223cc49471617e8
LW <lucaswankerl8@gmail.com>
2024-02-24 11:32:50 +0100

    modified:   src/Game/pages/Game.js

--- src/Game/pages/Game.js
+++ src/Game/pages/Game.js
@@ -8,15 +8,15 @@ import { fetchData } from './../api';
 
 function Game() {
   //Use States to decide which Component is rendered
-  const [yourTurn, setYourTurn] = useState(true);
+  const [yourTurn, setYourTurn] = useState();
   const [roundStarted, setRounStarted] = useState(false);
   const [roundEnded, setRounEnded] = useState(false);
   const [gameEnded, setGameEnded] = useState(false);
-  const [isSinglePlayer, setIsSinglePlayer] = useState(true);
+  const [isSinglePlayer, setIsSinglePlayer] = useState();
 
   //get the necessary data for the current game
   const [gameData, setGameData] = useState([]);
-  const [accessToken, setAccessToken] = useState("atsp2erd");
+  const [accessToken, setAccessToken] = useState("atsp1erd");
   const [dataFetched, setDataFetched] = useState(false);
   
   //stores current game Data
@@ -26,16 +26,19 @@ function Game() {
   const [currentQuestion, setCurrentQuestion] = useState();
   const [dataSet, setDataSet] = useState(false);
   
+  //fetch gameData
   useEffect(() => {
     fetchData('http://localhost:5000/gameData', setGameData, accessToken, setDataFetched)
   }, []);
 
+  //decide who is playing
   useEffect(() => {
     if(dataFetched){
     getCurrentPlayer(gameData, accessToken);
     }
   }, [dataFetched, gameData, player]);
 
+  //when you know who is playing, get the current question and category
   useEffect(() => {
     if(dataFetched){
     getCurrentQuestion(gameData, player);
@@ -43,19 +46,27 @@ function Game() {
     }
   }, [player]);
 
+  //when you know which question is played, you can calculate the turn number
   useEffect(() => {
     if(dataFetched){
-    setCurrentRound(Math.ceil(currentQuestion / 3))
+    setCurrentRound(Math.ceil(currentQuestion / 3));
+    decideTurn(gameData,player);
     setDataSet(true);
     }
   }, [currentQuestion]);
 
+  //relevant data is set
   useEffect(() => {
     if(dataSet){
     console.log("Player: " + player + " Runde: " + currentRound + " Frage: " + currentQuestion + " Kategorie: " + questionCategory)
     console.log(gameData)
+    console.log(yourTurn)
     }
-  }, [dataSet, gameData, player]);
+  }, [dataSet]);
+
+  useEffect(() => {
+    console.log(yourTurn)
+  }, [yourTurn]);
 
   function getCurrentPlayer(gameData, accessToken) {
     if (accessToken === gameData[1].AccessTokenOne){
@@ -73,9 +84,9 @@ function Game() {
       if (question["AnswerPlayer" + player] === null) {
         setCurrentQuestion(question["QuestionNumber"]);
         questionFound = true; // Set flag to true after first occurrence
+        return question["QuestionNumber"];
       }
     }
-    return null;
   }
   
   function getCurrentCategory(gameData, player) {
@@ -89,6 +100,25 @@ function Game() {
     }
     return null;
   }
+
+  //entscheidet welcher Spieler am Zug ist
+  function decideTurn(gameData, player, isSinglePlayer) {
+    const Player1Round = Math.ceil(getCurrentQuestion(gameData, 1) / 3);
+    const Player2Round = Math.ceil(getCurrentQuestion(gameData, 2) / 3);
+
+    if (!isSinglePlayer) {
+        if (player === 1 && (Player1Round === 1 || Player1Round === Player2Round - 1)) {
+            setYourTurn(true);
+        } else if (player === 2 && Player2Round === Player1Round - 1) {
+            setYourTurn(true);
+        } else {
+            setYourTurn(false);
+        }
+    } else {
+        setYourTurn(true);
+    }
+}
+
   
 
   return (
