0daf95344f6974bef9e8d46e125705f95cc7d580
matthiasmulske <matthias.mulske@web.de>
2024-03-03 19:02:29 +0100

    new Headline

--- src/Components/LoginMaske.js
+++ src/Components/LoginMaske.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
+import GoogleButton from 'react-google-button';
 
 function LoginMaske() {
   const [username, setUsername] = useState('');
@@ -23,6 +24,82 @@ function LoginMaske() {
     console.log("Registrierung für:", username, password);
     alert('Dies ist nur ein Prototyp. Aus diesem Grund funktioniert der Button nicht');
   }
+
+
+  const googleButtonStyle = {
+    backgroundColor: 'blue',
+    color: 'white',
+    border: 'none',
+    padding: '0px 20px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    marginTop: '20px'
+  };
+
+  const styles = {
+    pageContainer: {
+      position: 'relative', 
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '75vh',
+    },
+    loginContainer: {
+      textAlign: 'center',
+      width: '21%',
+      border: '1px solid #ccc',
+      padding: '20px',
+      borderRadius: '5px',
+      boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+      position: 'relative', 
+    },
+    formGroup: {
+      marginBottom: '20px',
+      textAlign: 'left',
+    },
+    inputField: {
+      width: '100%',
+      padding: '8px',
+      fontSize: '16px',
+      border: '1px solid #ccc',
+      borderRadius: '5px',
+      boxSizing: 'border-box',
+    },
+    buttonContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    button: {
+      padding: '10px 20px',
+      fontSize: '18px',
+      backgroundColor: '#007bff',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      marginLeft: '10px',
+      boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+    },
+    errorMessage: {
+      color: 'red',
+      marginTop: '10px',
+    },
+    messageContainer: {
+      position: 'absolute',
+      top: '20px',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      backgroundColor: '#f0f0f0',
+      padding: '20px',
+      borderRadius: '5px',
+      boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+    },
+    linkContainer: {
+      position: 'absolute',
+      top: '20px',
+      right: '20px',
+    },
+  };
 
   return (
     <div style={styles.pageContainer}>
@@ -52,6 +129,7 @@ function LoginMaske() {
               <button type="submit" style={styles.button}>Anmelden</button>
               <button type="button" onClick={handleRegister} style={styles.button}>Registrieren</button>
             </div>
+            <GoogleButton onClick={() => { console.log('Google-Schaltfläche angeklickt') }} style={googleButtonStyle} />
           </form>
         </div>
         {errorMessage && (
@@ -67,70 +145,5 @@ function LoginMaske() {
     </div>
   );
 }
-
-const styles = {
-  pageContainer: {
-    position: 'relative', 
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '75vh',
-  },
-  loginContainer: {
-    textAlign: 'center',
-    width: '21%',
-    border: '1px solid #ccc',
-    padding: '20px',
-    borderRadius: '5px',
-    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
-    position: 'relative', 
-  },
-  formGroup: {
-    marginBottom: '20px',
-    textAlign: 'left',
-  },
-  inputField: {
-    width: '100%',
-    padding: '8px',
-    fontSize: '16px',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    boxSizing: 'border-box',
-  },
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  button: {
-    padding: '10px 20px',
-    fontSize: '18px',
-    backgroundColor: '#007bff',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    marginLeft: '10px',
-    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
-  },
-  errorMessage: {
-    color: 'red',
-    marginTop: '10px',
-  },
-  messageContainer: {
-    position: 'absolute',
-    top: '20px',
-    left: '50%',
-    transform: 'translateX(-50%)',
-    backgroundColor: '#f0f0f0',
-    padding: '20px',
-    borderRadius: '5px',
-    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
-  },
-  linkContainer: {
-    position: 'absolute',
-    top: '20px',
-    right: '20px',
-  },
-};
 
 export default LoginMaske;
