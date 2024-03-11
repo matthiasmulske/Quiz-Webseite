5de91227591f87d7c119927672ed377e66b3a63c
matthiasmulske <matthias.mulske@web.de>
2024-03-11 20:48:40 +0100

    new Headline

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -105,7 +105,7 @@ export default function Navbar({isLoggedIn, setIsLoggedIn, user}) {
                 open={Boolean(anchorEl)}
                 onClose={handleCloseMenu}
               >
-                <MenuItem onClick={handleCloseMenu}>NutzerID: {user}</MenuItem>
+                <MenuItem onClick={handleCloseMenu}>Benutzerprofil: {user}</MenuItem>
                 <MenuItem onClick={handleCloseMenu}>Benutzername ändern</MenuItem>
                 <MenuItem onClick={handleCloseMenu}>Passwort ändern</MenuItem>
               </Menu>
